3d1a10d0e4443547d9cdcfda69422579f16770c8
shan5742 <asamshan456@gmail.com>
2023-04-20 23:09:43 +0100

    add testimonials

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
 import { useState } from 'react'
 import techLeapLogo from './assets/techleaplong.svg'
 import './App.css'
+import { td } from './data/testimonials';
+
+type TestimonialProps = {
+  quote: string
+  author: string
+  title: string
+}
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+
+  const magicLink = `https://magic.beehiiv.com/v1/53ff64cb-69f4-4bc1-9ea4-3c75252cea93?email=${email}&redirect_to=https://techleaphq.com/`
+  const sub = () => {
+    if (!email) return
+    window.location.replace(magicLink)
+  }
+
+  const Testimonial = ({ quote, author, title }: TestimonialProps) => {
+    return (
+      <div key={author} className="testimonial">
+        <h1 className="quote">{`"${quote}"`}</h1>
+        <p className="author">
+          - {author},<span> {title}</span>
+        </p>
+      </div>
+    );
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="#" target="_blank">
-          <img src={techLeapLogo} className="logo" alt="Tech Leap logo" />
+      <div id="hero" className="hero">
+        <div className="hero-content">
+          <div>
+            <a href="#" target="_blank">
+              <img src={techLeapLogo} className="logo" alt="Tech Leap logo" />
+            </a>
+          </div>
+          <div className="content">
+            <p className="text">Practical guidance for breaking into software development and thriving in your early years.</p>
+            <input className='input'
+              type="email"
+              placeholder="email address"
+              onChange={e => setEmail(e.target.value)} />
+            <button className='button' onClick={sub}>Subscribe</button>
+          </div>
+        </div>
+        <a href="#testimonials" className="scroll">
+          <p className="subText">See what subscribers are saying</p>
+          <p className="emoji">👇</p>
         </a>
       </div>
-      <div className="content">
-        <p className="text">Practical guidance for breaking into software development and thriving in your early years.</p>
-        <iframe
-          className="iframe"
-          src="https://embeds.beehiiv.com/0c7500a7-6ab9-480b-8736-873ee23fd5e9?slim=true"
-          data-test-id="beehiiv-embed"
-          />
+      <div id="testimonials" className="testimonials">
+        {td.map((t) => (
+          <Testimonial key={t.author} author={t.author} quote={t.quote} title={t.title} />
+        ))}
+        <div className="flex">
+          <input placeholder='email address' className="input" type="email"
+          onChange={e => setAddress(e.target.value)} />
+          <button className="button">subscribe</button>
+        </div>
       </div>
     </div>
   )
